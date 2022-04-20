3648fbed50aad5a7558138eda81df1bd64ddd8f3
zdulak <dpzdulski@gmail.com>
2022-04-20 11:22:09 +0200

    Add KafkConsumerApp

---
+++ src/main/scala/KafkaConsumerApp.scala
@@ -0,0 +1,24 @@
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import java.time.Duration
+import java.util
+import java.util.Properties
+import scala.jdk.CollectionConverters._
+
+object KafkaConsumerApp extends App {
+  val props = new Properties()
+  props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+  props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("group.id", "my-group")
+//  props.put("enable.auto.commit", "true")
+//  props.put("auto.commit.interval.ms", "1000")
+
+  val consumer = new KafkaConsumer[String, String](props)
+  consumer.subscribe(List("my-topic").asJava)
+  while (true) {
+    val records = consumer.poll(Duration.ofMillis(100)).asScala
+    for (record <- records) {
+      println(record.offset, record.key, record.value)
+    }
+  }
+}
